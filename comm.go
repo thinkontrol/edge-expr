c21959b8715c9ea9f95a139871aba0ee82b3cd14
ShenYang <shenyang.sx@outlook.com>
2025-10-25 09:52:55 +0800

    add mapstructure

--- comm.go
+++ comm.go
@@ -5,22 +5,22 @@ import (
 )
 
 type PushValue struct {
-	Key       string     `json:"key"`
-	Value     any        `json:"value"`
-	Timestamp *time.Time `json:"timestamp,omitempty"`
+	Key       string     `json:"key" mapstructure:"key"`
+	Value     any        `json:"value" mapstructure:"value"`
+	Timestamp *time.Time `json:"timestamp,omitempty" mapstructure:"timestamp"`
 }
 
 type Command struct {
-	CommandID string         `json:"command_id"`
-	Command   string         `json:"command"`
-	Payload   map[string]any `json:"payload,omitempty"`
-	Timestamp *time.Time     `json:"timestamp,omitempty"`
+	CommandID string         `json:"command_id" mapstructure:"command_id"`
+	Command   string         `json:"command" mapstructure:"command"`
+	Payload   map[string]any `json:"payload,omitempty" mapstructure:"payload"`
+	Timestamp *time.Time     `json:"timestamp,omitempty" mapstructure:"timestamp"`
 }
 
 type CommandResponse struct {
-	CommandID string         `json:"command_id"`
-	Message   string         `json:"message,omitempty"`
-	Success   bool           `json:"success"`
-	Payload   map[string]any `json:"payload,omitempty"`
-	Timestamp *time.Time     `json:"timestamp,omitempty"`
+	CommandID string         `json:"command_id" mapstructure:"command_id"`
+	Message   string         `json:"message,omitempty" mapstructure:"message"`
+	Success   bool           `json:"success" mapstructure:"success"`
+	Payload   map[string]any `json:"payload,omitempty" mapstructure:"payload"`
+	Timestamp *time.Time     `json:"timestamp,omitempty" mapstructure:"timestamp"`
 }
